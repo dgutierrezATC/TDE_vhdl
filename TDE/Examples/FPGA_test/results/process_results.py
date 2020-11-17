3c19dd624b6fa1b123bf5c089e55860ad8297a87
Daniel Gutierrez Galan <dgutierrez@atc.us.es>
2020-11-17 14:31:09 +0100

    Updated

--- TDE/Examples/FPGA_test/results/process_results.py
+++ TDE/Examples/FPGA_test/results/process_results.py
@@ -1,10 +1,16 @@
 import csv
 import math
 import numpy
+import os
+
+project_path = os.getcwd()
+#print(project_path)
+test_folder_path = "test_0001\\"
+test_filename = "mili.csv"
 
 ts = []
 
-with open('results_behavioral_simulation2.txt') as csv_file:
+"""with open(test_folder_path+test_filename) as csv_file:
 	csv_reader = csv.reader(csv_file, delimiter=',')
 	line_count = 0
 	for row in csv_reader:
@@ -15,21 +21,39 @@ with open('results_behavioral_simulation2.txt') as csv_file:
 		ts.append(ts_us)
 		#print(float(row[0]))
 		line_count += 1
+print("Processed lines: " + str(line_count))"""
+
+with open(test_folder_path+test_filename) as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=';')
+	line_count = 0
+	for row in csv_reader:
+		if(line_count > 0):
+			ts_raw = row[0]
+			ts_us = float(ts_raw)
+			if(ts_us == 0.0 and line_count > 5):
+				print("zero")
+			else:
+				ts.append(ts_us)
+		#print(float(row[0]))
+		line_count += 1
 print("Processed lines: " + str(line_count))
 
 event_counter = 0
 event_counter_list = []
-time_dif_th = 500000.0
+time_dif_th = 200000.0
 
 for index in range(len(ts)-1):
 	time_dif = ts[index + 1] - ts[index]
 	if(time_dif <= time_dif_th):
 		event_counter = event_counter + 1
 	else:
+		event_counter = event_counter + 1
 		event_counter_list.append(event_counter)
 		event_counter = 0
-		
-print("Peaks detected: " + str(len(event_counter_list)))
+
+print("Num peaks detected: " + str(len(event_counter_list)))		
+print("Peaks detected: ")
+print(event_counter_list)
 print("Max value: " + str(numpy.max(event_counter_list)))
 print("Min value: " + str(numpy.min(event_counter_list)))
 print("Mean value: " + str(numpy.mean(event_counter_list)))
