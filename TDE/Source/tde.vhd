3bbd5aa80206798b6020826a04a289254817336c
Daniel Gutierrez Galan <dgutierrez@atc.us.es>
2020-11-17 14:37:38 +0100

    Modified the header

--- TDE/Source/tde.vhd
+++ TDE/Source/tde.vhd
@@ -1,6 +1,6 @@
 --/////////////////////////////////////////////////////////////////////////////////
 --//                                                                             //
---//    Copyright © 2020  Daniel Gutierrez-Galan                                 //
+--//    Copyright (c) 2020  Daniel Gutierrez-Galan                               //
 --//                                                                             //
 --//    This file is part of the TDE_vhdl project.                               //
 --//                                                                             //
